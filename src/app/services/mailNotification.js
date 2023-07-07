5f8df2f1f7e13c4a77487cc18aca254e3eb1f89b
Chetan7528 <75465248+Chetan7528@users.noreply.github.com>
2023-07-07 12:43:09 +0530

    notification issue fixed

--- src/app/services/mailNotification.js
+++ src/app/services/mailNotification.js
@@ -29,12 +29,12 @@ const sendMail = async (to, subject, html, attachments) => {
 
 module.exports = {
   welcomeMail: async (details) => {
-    const html = `<div> \r\n<p>Hello,<\/p>\r\n\r\n<p> Welcome to SwiftGuard. <\/p>\r\n\r\n<p>You recently created a SwiftGuard Account. <\/p>\r\n\r\n<p>Your SwiftGuard Registered Mail is: <b>${details.email} <\/b><\/p>\r\n\r\n<p><\/br>Thanks,<\/p>\r\n\r\n<p><b>The SwiftGuard Account Team<\/b><\/p>\r\n<\/div>`;
-    await sendMail(details.email, "Welcome to SwiftGuard", html);
+    const html = `<div> \r\n<p>Hello,<\/p>\r\n\r\n<p> Welcome to Gurubox. <\/p>\r\n\r\n<p>You recently created a Gurubox Account. <\/p>\r\n\r\n<p>Your Gurubox Registered Mail is: <b>${details.email} <\/b><\/p>\r\n\r\n<p><\/br>Thanks,<\/p>\r\n\r\n<p><b>The Gurubox Account Team<\/b><\/p>\r\n<\/div>`;
+    await sendMail(details.email, "Welcome to Gurubox", html);
   },
   sendOTPmail: async ({ email, code }) => {
     try {
-      const html = `<div> \r\n<p>Password Reset Instructions<\/p>\r\n\r\n<p>Your SwiftGuard One-Time password reset code is: ${code}. Enter online when prompted. This passcode will expire in 5 minutes<\/p><\/br>Thank you for updating your password.<\/p>\r\n\r\n<p><b>SwiftGuard<\/b><\/p>\r\n<\/div>`;
+      const html = `<div> \r\n<p>Password Reset Instructions<\/p>\r\n\r\n<p>Your Gurubox One-Time password reset code is: ${code}. Enter online when prompted. This passcode will expire in 5 minutes<\/p><\/br>Thank you for updating your password.<\/p>\r\n\r\n<p><b>Gurubox<\/b><\/p>\r\n<\/div>`;
       return await sendMail(email, "Password Reset Instructions", html);
     } catch (err) {
       console.log(err);
@@ -43,7 +43,7 @@ module.exports = {
   },
   passwordChange: async ({ email }) => {
     try {
-      const html = `<div> Your password has been reset, if you didn't update your password, please call us on (.) between 9am - 5pm Monday to Friday. \r\n\r\nSwiftGuard  </div>`;
+      const html = `<div> Your password has been reset, if you didn't update your password, please call us on (.) between 9am - 5pm Monday to Friday. \r\n\r\nGurubox  </div>`;
       return await sendMail(email, "PASSWORD RESET NOTIFICATION EMAIL", html);
     } catch (err) {
       console.log(err);
